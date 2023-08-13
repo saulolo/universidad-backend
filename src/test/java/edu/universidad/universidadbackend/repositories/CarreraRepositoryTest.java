7853c0895214145a22b067a23f3d7f9f2473d578
Saul Echeverri <secheverri@cidenet.com.co>
2023-08-13 17:42:00 -0500

    Configurando el aplication.properties del package de Test. Iniciando con los test de los repositorios Clase 56.

---
+++ src/test/java/edu/universidad/universidadbackend/repositories/CarreraRepositoryTest.java
@@ -0,0 +1,24 @@
+package edu.universidad.universidadbackend.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarreraRepositoryTest {
+
+    @Test
+    void findCarrerasByNombreContains() {
+    }
+
+    @Test
+    void findCarrerasByNombreContainsIgnoreCase() {
+    }
+
+    @Test
+    void findCarrerasByCantidadAniosAfter() {
+    }
+
+    @Test
+    void findCarrerasByProfesorNombreYApellido() {
+    }
+}
