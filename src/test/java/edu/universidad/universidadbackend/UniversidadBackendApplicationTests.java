65231277a41f6383eb9dc622c70d62e8217bf2c7
Saul Echeverri <secheverri@cidenet.com.co>
2023-08-13 16:23:41 -0500

    Introducción a pruebas unitarias Clase 55.

--- src/test/java/edu/universidad/universidadbackend/UniversidadBackendApplicationTests.java
+++ src/test/java/edu/universidad/universidadbackend/UniversidadBackendApplicationTests.java
@@ -1,13 +1,47 @@
 package edu.universidad.universidadbackend;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class UniversidadBackendApplicationTests {
 
+	Calculadora calculadora = new Calculadora();
+
+
+	@Test  //Anotación necesaria para ejecutar un test
+	@DisplayName("Suma de valorA y de valorB") //Personalizar el nombre del test
+	void sumaDeValores() {
+
+		/* ARRENGE */
+
+		//Given: Definimos nuestro contexto (precondición)
+		int valorA = 2;
+		int valorB = 3;
+
+		//when: Ejecutamos lo que queremos probar
+		int expectativa = calculadora.sumar(valorA, valorB);
+
+		//then: Las validadciones
+		int resultadoExperado = 5;
+		/* ASSERT */
+		assertThat(expectativa).isEqualTo(resultadoExperado);
+
+	}
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Test deprecado")
+	@Disabled("Test deprecado") //Apara indicar que este test esta desactivado
+	void testDesactivado() {
+
+	}
+
+	class Calculadora{
+		int sumar(int valor_a, int valor_b){
+			return valor_a + valor_b;
+		}
 	}
 
 }
